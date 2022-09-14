d1a0f8d2f3325fc8206aa2388f82f185e94acc89
Devbakori <109731775+Devbakori@users.noreply.github.com>
2022-09-14 15:54:38 +0530

    Add files via upload

---
+++ Task/area.c
@@ -0,0 +1,16 @@
+#include<stdio.h> 
+int main()
+{
+	float area,r,l,h,t,rectangla,triangle;
+	printf("enter any no of Aera"); 
+	scanf("%f",&r); area=3.14*r*r; 
+	printf("\n value of area :%f",area);
+	printf("\n enter any 2 no of rectangla:"); 
+	scanf("%f %f",&l,&h); 
+	printf("\n value of rectangla a:rectangla=1*h;%f",rectangla);
+	printf("\n enter any no of triangle");
+	scanf("%f",&t);
+	triangle=t*t*t; 
+	printf("\n value of triangle a:%f",triangle);
+
+}
