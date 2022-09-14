e7db451f85433db03cbe4a4f68134f9c8a166b14
Devbakori <109731775+Devbakori@users.noreply.github.com>
2022-09-14 15:48:07 +0530

    Add files via upload

---
+++ assignment/module3.1/name.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	printf("\n my name is dev bakori.");
+	printf("\n my date of birth is 21st april 2001.");	
+	printf("\n i am 20 years old.");
+	printf("\n i am living in jamjodhpur.");
+	return 0;			
+}
+
